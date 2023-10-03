73da47b7f6554a0fbcf4b82401e745bdcffd2f60
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2023-10-03 10:12:03 -0300

    feat(feed): cria componente FeedPost estatico

--- src/app/(pages)/feed/page.tsx
+++ src/app/(pages)/feed/page.tsx
@@ -1,3 +1,13 @@
+import FeedPost from "../../components/FeedPost";
+
 export default function Feed() {
-  return <div className="flex">Feed</div>;
+  return (
+    <div className="flex max-w-[945px] mx-auto mt-16">
+      <ul className="w-full">
+        {[1, 2, 3, 4].map((feed) => (
+          <FeedPost key={feed} />
+        ))}
+      </ul>
+    </div>
+  );
 }
