fcd242a43b1f2d3ce0d1ceadffa214650e865353
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2023-10-03 15:53:40 -0300

    feat(profile): cria pagina de overview mockada

--- src/app/(pages)/profile/overview/page.tsx
+++ src/app/(pages)/profile/overview/page.tsx
@@ -1,7 +1,43 @@
 export default function ProfileOverview() {
   return (
-    <div className="w-full bg-primary-default rounded-lg p-[30px]">
-      <span className="text-white text-2xl">Overview</span>
+    <div className="w-full bg-primary-default rounded-lg p-[30px] space-y-6">
+      <div className="flex">
+        <span className="w-1/3 font-medium text-neutral-gray-tertiary text-[13px]">
+          Full Name
+        </span>
+
+        <span className="w-2/3 text-white font-medium text-sm">
+          RAMON RAMOS EXT
+        </span>
+      </div>
+
+      <div className="flex">
+        <span className="w-1/3 font-medium text-neutral-gray-tertiary text-[13px]">
+          E-mail
+        </span>
+
+        <span className="w-2/3 text-white font-medium text-sm">
+          ramon_ramos_ext@carrefour.com
+        </span>
+      </div>
+
+      <div className="flex">
+        <span className="w-1/3 font-medium text-neutral-gray-tertiary text-[13px]">
+          Country
+        </span>
+
+        <span className="w-2/3 text-white font-medium text-sm">BR</span>
+      </div>
+
+      <div className="flex py-5">
+        <span className="w-1/3 font-medium text-neutral-gray-tertiary text-[13px]">
+          Username/Nick
+        </span>
+
+        <span className="w-2/3 text-white font-medium text-sm">
+          ramon_ramos_ext
+        </span>
+      </div>
     </div>
   );
 }
--- src/app/components/ProfileLinks.tsx
+++ src/app/components/ProfileLinks.tsx
@@ -18,12 +18,12 @@ export default function ProfileLinks() {
   const pathname = usePathname();
 
   return (
-    <div className="flex mt-8 space-x-5">
+    <div className="flex mt-8 space-x-7">
       {links.map((link) => {
         const isActive = pathname === link.href;
 
         return (
-          <Link key={link.href} className="flex relative h-14" href={link.href}>
+          <Link key={link.href} className="flex relative h-10" href={link.href}>
             <span
               className={`text-[15px] font-semibold  ${
                 isActive ? "text-[#3699ff]" : "text-neutral-gray-tertiary"
@@ -35,7 +35,7 @@ export default function ProfileLinks() {
             <div
               className={`absolute left-0 bottom-0 h-[2px] bg-[#3699ff] transition-all ${
                 isActive ? "w-full" : "w-0"
-              } `}
+              }`}
             />
           </Link>
         );
